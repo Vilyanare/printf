69e80c4cc62f0f3719a247fb25d6bbf046ea21bd
Vilyanare <acolyteofwar@gmail.com>
2017-10-22 03:39:53 +0000

    Changed puts back to normal

--- _puts.c
+++ _puts.c
@@ -4,16 +4,12 @@
  *@str: string to be printed
  *Return: if % encountered return address of %
  */
-char *_puts(char *str)
+void _puts(char *str)
 {
 	int x;
 
 	for (x = 0; str[x]; x++)
 	{
-		if (str[x] == '%')
-			return (str + x);
 		_putchar(str[x]);
 	}
-	_putchar('\n');
-	return(NULL);
 }
--- holberton.h
+++ holberton.h
@@ -5,6 +5,6 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-char *_puts(char *str);
+void _puts(char *str);
 
 #endif
