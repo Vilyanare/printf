cba833a56953d513591c4bf8753d8116d0372e49
Vilyanare <acolyteofwar@gmail.com>
2017-10-24 22:01:28 +0000

    updated test file

--- test.c
+++ test.c
@@ -20,6 +20,7 @@ int main(void)
 /*    addr = (void *)0x7ffe637541f0;
 */    _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Binary:[%b]\n", ui);
     _printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
    _printf("Unsigned:[%u]\n", ui);
